44eb8b66a9799a04759225eda211918a28b03c72
OlenaLotos <94827284+OlenaLotos@users.noreply.github.com>
2022-06-08 10:53:32 +0000

    Deploying to gh-pages from @ OlenaLotos/goit-js-hw-10@bc8860a3570879c815bd3fa2b665dd602e7212a6 🚀

---
+++ index.77b99f8d.js
@@ -0,0 +1,2 @@
+document.querySelector(".country-list");fetch("https://restcountries.com/v3.1/name/peru").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
+//# sourceMappingURL=index.77b99f8d.js.map
---
+++ index.f9f5abd1.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".country-list");fetch("https://restcountries.com/v3.1/name/peru").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}))}();
+//# sourceMappingURL=index.f9f5abd1.js.map
