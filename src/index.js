bc8860a3570879c815bd3fa2b665dd602e7212a6
OlenaLotos <olenameshanych@gmail.com>
2022-06-08 13:52:55 +0300

    start task 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/styles.css';
+// import debounce from 'lodash.throttle';
+
+const countryInfo = document.querySelector('.country-list');
 
 const DEBOUNCE_DELAY = 300;
+
+
+// fetchCountries('https://restcountries.com/v3.1/name/{name}')
+
+fetch('https://restcountries.com/v3.1/name/peru')
+    .then(response => {
+    
+    return response.json();
+    })
+    .then(country => {
+    console.log(country);
+    })
+    .catch(error => {
+    console.log(error)
+});
+
+function makeCountryMarkup({ name, capital, population, flags, languages }) {
+    countryInfo.classList.remove('is-hidden');
+    return ``
+}
+
+// https://restcountries.com/v2/{service}?fields={field},{field},{field}
